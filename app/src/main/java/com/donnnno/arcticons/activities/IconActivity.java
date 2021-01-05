5a7318dcb1371af069c5a715209ef2280c84c2d5
DannyGJdeJong <dannygjdejong@gmail.com>
2021-01-05 23:28:54 +0100

    Added a search bar

--- app/src/main/java/com/donnnno/arcticons/activities/IconActivity.java
+++ app/src/main/java/com/donnnno/arcticons/activities/IconActivity.java
@@ -2,17 +2,22 @@ package com.donnnno.arcticons.activities;
 
 import android.graphics.Rect;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
+import android.widget.EditText;
 
 import com.donnnno.arcticons.R;
 import com.donnnno.arcticons.utils.ImageUtils;
 import com.donnnno.arcticons.utils.ScreenUtils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class IconActivity extends com.donnnno.arcticons.activities.BaseActivity {
     private ArrayList<LinearLayout> layoutList = new ArrayList<>();
@@ -35,7 +40,6 @@ public class IconActivity extends com.donnnno.arcticons.activities.BaseActivity 
         LinearLayout.LayoutParams baseParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
         LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         LinearLayout.LayoutParams containerParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        LinearLayout.LayoutParams imageParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT, 1f);
 
         ScrollView baseScroller = new ScrollView(this);
         baseScroller.setLayoutParams(baseParams);
@@ -54,7 +58,51 @@ public class IconActivity extends com.donnnno.arcticons.activities.BaseActivity 
         baseLayout.setPadding(margin, margin, 0, 0);
         baseScroller.addView(baseLayout);
 
+        LinearLayout imageLayout = new LinearLayout(this);
+        imageLayout.setOrientation(LinearLayout.VERTICAL);
+        imageLayout.setLayoutParams(containerParams);
+        imageLayout.setPadding(margin, margin, 0, 0);
+
+        EditText searchBar = new EditText(this);
+        searchBar.setLayoutParams(layoutParams);
+        searchBar.setPadding(margin, margin, 0, 0);
+        searchBar.setMaxLines(1);
+        searchBar.setLines(1);
+        searchBar.setInputType(InputType.TYPE_CLASS_TEXT);
+        searchBar.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                selectImages(windowWidth, margin, imageLayout, width, charSequence.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+
+        });
+        baseLayout.addView(searchBar);
+        baseLayout.addView(imageLayout);
+
+        selectImages(windowWidth, margin, imageLayout, width, "");
+    }
+
+    private void selectImages(int windowWidth, int margin, LinearLayout baseLayout, int width, String searchString) {
+        layoutList = new ArrayList<>();
+        imageList = new ArrayList<>();
+        baseLayout.removeAllViews();
+
+        LinearLayout.LayoutParams containerParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        LinearLayout.LayoutParams imageParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT, 1f);
+
         String[] images = getResources().getStringArray(R.array.icon_pack);
+        images = Arrays.stream(images).filter(s -> s.contains(searchString.toLowerCase())).toArray(String[]::new);
+
         for (int i = 0; i < images.length; i++) {
             if ((i % width) == 0) {
                 layoutList.add((i / width), new LinearLayout(this));
