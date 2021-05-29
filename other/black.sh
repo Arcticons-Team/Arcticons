e437719ec3a5dad3c921955ba085eba52e2ceee2
Onno <onnovdd@protonmail.com>
2021-05-30 01:56:07 +0200

    Update black.sh

--- other/black.sh
+++ other/black.sh
@@ -2,13 +2,15 @@
 # batch_invert_color.sh
 
 white="stroke\s*:\s*\(#fff\|#ffffff\|white\)"
+
+black="stroke:#000"
+for x in *.svg; do
+	sed -i "s/${white}/${black}/Ig" "${x}"
+done
+
 white="fill\s*:\s*\(#fff\|#ffffff\|white\)"
 
-# If you want to replace all stroke colors (won't match rgb()/hsl()/..., but will match #... and black/blue/...):
-# ="stroke\s*:\s*#\?[[:alnum:]]\+"
-
-black="stroke:#000" # can replace with any target color
-black="fill:#000" # can replace with any target color
+black="fill:#000"
 for x in *.svg; do
 	sed -i "s/${white}/${black}/Ig" "${x}"
 done
