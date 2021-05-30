0ac8e6ccc4264c84be920ebb49e9c6f0a2100d93
Onno <onnovdd@protonmail.com>
2021-05-30 13:56:10 +0200

    new icons

--- other/white.sh
+++ other/white.sh
@@ -3,9 +3,9 @@
 for DIR in $(find -name "*.svg")
 do
   FILE=${DIR##*/}
-  sed -r -i 's:fill="#\w{3,6}":fill="#fff":' ${FILE}
-  # A check is needed in case some files have both a style and fill attribute
-  if [ $(grep -o fill= ${FILE} | wc -l) -ge 1 ]
+  sed -r -i 's:stroke="#\w{3,6}":fill="#fff":' ${FILE}
+
+  if [ $(grep -o stroke= ${FILE} | wc -l) -ge 1 ]
   then
     sed -r -i 's/style="[^"]*"//' ${FILE}
   else
