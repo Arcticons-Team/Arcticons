65b1bf9445f326a4c42df9df8252689d1457ad73
Donno <31142286+Donnnno@users.noreply.github.com>
2021-09-10 10:51:23 +0200

    small fixes

--- other/rasterdark.sh
+++ other/rasterdark.sh
@@ -9,28 +9,12 @@ do
   FILE=${DIR##*/}
   NAME=${FILE%.*}
   cp ${FILE} ${FILE}.tmp
-  scour --remove-descriptive-elements --enable-id-stripping --enable-viewboxing --enable-comment-stripping --nindent=4 -i ${FILE}.tmp -o ${FILE}
   rm ${FILE}.tmp
   cp -f ${FILE} ${ICON}/${FILE}
   for SIZE in ${SIZES}
   do
     inkscape --export-filename=${NAME}.png --export-width=${SIZE} --export-height=${SIZE} ${NAME}.svg
     case ${SIZE} in
-      48)
-	mv ${NAME}.png ${EXPORT}/drawable-mdpi/
-	;;
-      72)
-	mv ${NAME}.png ${EXPORT}/drawable-hdpi/
-	;;
-      96)
-	mv ${NAME}.png ${EXPORT}/drawable-xhdpi/
-	;;
-      144)
-	mv ${NAME}.png ${EXPORT}/drawable-xxhdpi/
-	;;
-      192)
-	mv ${NAME}.png ${EXPORT}/drawable-xxxhdpi/
-	;;
       256)
 	mv ${NAME}.png ${EXPORT}/drawable-nodpi/
 	;;
