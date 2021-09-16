f59b9c3cf8f5eb9caa0971bca17a8bbd83ff984d
Aman9das <amandas62640@gmail.com>
2021-09-16 20:35:07 +0530

    nuke redundand app folder completely :)

--- libs/app/src/main/java/com/candybar/dev/services/ClockWidget.java
+++
@@ -1,6 +0,0 @@
-package com.candybar.dev.services;
-
-import candybar.lib.services.CandyBarWidgetService;
-
-public class ClockWidget extends CandyBarWidgetService {
-}
--- libs/app/src/main/java/com/candybar/dev/services/MuzeiService.java
+++
@@ -1,6 +0,0 @@
-package com.candybar.dev.services;
-
-import candybar.lib.services.CandyBarMuzeiService;
-
-public class MuzeiService extends CandyBarMuzeiService {
-}
