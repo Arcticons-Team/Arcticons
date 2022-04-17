655723d7bc1057c6c1e44b7409e50d5aabe1cbee
Konstantin Tutsch <konstantin.tutsch@e.email>
2022-04-17 13:10:15 +0200

    Script verbose

--- other/f-droid_check.sh
+++ other/f-droid_check.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 links=$(grep "https://f-droid.org/*" $1)
+repeat=0
 
 for link in ${links}; do
+	printf "Not found (since last success): $repeat\r"
+
 	if curl --output /dev/null --silent --head --fail "$link"; then
-		echo "$link"
+		printf "\rFound: $link\n"
+		repeat=0
+	else
+		repeat=$((repeat+1))
 	fi
 done
