33a6415227909abdaf28c1774a0caca8d4a427f1
Bastian <8929967+Kaiserdragon2@users.noreply.github.com>
2023-01-08 13:51:53 +0100

    Make everything working
    Also includes a check if icons are white if not it prints out the ones that are not white and stops the script

--- other/scripts/powerscript.py
+++ other/scripts/powerscript.py
@@ -15,28 +15,36 @@ from shutil import move
 
 parser = argparse.ArgumentParser()
 parser.add_argument('SVG_DIR', type=str, help='directory containing the SVG files')
-parser.add_argument('APP_MAIN_DIR', type=str, help='main app directory somthing like app/src/main')
+parser.add_argument('APP_SRC_DIR', type=str, help='main app directory somthing like app/src/main')
+parser.add_argument('ICONS_DIR', type=str, help='directory that contains the folders for the black and white svg')
 args = parser.parse_args()
 
 SVG_DIR = str(pathlib.Path(args.SVG_DIR).resolve())
-APP_MAIN_DIR = str(pathlib.Path(args.APP_MAIN_DIR).resolve())
-icon_dir = SVG_DIR + "\icon"
-icon2_dir = SVG_DIR + "\icon2"
-export_white_dir = SVG_DIR + "/white"
-export_black_dir = SVG_DIR + "/black"
-export_xml_dir = SVG_DIR + "/app"
-ASSETS_PATH = APP_MAIN_DIR + "/assets"
+APP_SRC_DIR = str(pathlib.Path(args.APP_SRC_DIR).resolve())
+ICONS_DIR = str(pathlib.Path(args.ICONS_DIR).resolve())
+
+#Define Path
+APPFILTER_PATH = SVG_DIR + "/appfilter.xml"
+DRAWABLE_PATH = APP_SRC_DIR + "/main/res/xml/drawable.xml"
+NEWDRAWABLE_PATH = SVG_DIR + "/newdrawables.xml"
+WHITE_DIR = ICONS_DIR + "/white"
+BLACK_DIR = ICONS_DIR + "/black"
+EXPORT_DARK_DIR = APP_SRC_DIR +"/dark/res/drawable-nodpi"
+EXPORT_LIGHT_DIR = APP_SRC_DIR +"/light/res/drawable-nodpi"
+RES_XML_PATH = APP_SRC_DIR + "/main/res/xml"
+ASSETS_PATH = APP_SRC_DIR + "/main/assets"
+VALUE_PATH = APP_SRC_DIR + "/main/res/values"
 
 #Export Sizes of the icons
 SIZES = [256]
 #Define original color
-ORIGINAL_STROKE = r"replace_stroke\s*:\s*(#ffffff|#fff|white)"
-ORIGINAL_FILL = r"replace_fill\s*:\s*(#ffffff|#fff|white)"
+ORIGINAL_STROKE = r"stroke\s*:\s*(#ffffff|#fff|white)"
+ORIGINAL_FILL = r"fill\s*:\s*(#ffffff|#fff|white)"
 #Define Replace Colors
-REPLACE_STROKE_WHITE = "replace_stroke:#fff"
-REPLACE_FILL_WHITE = "replace_fill:#fff"
-REPLACE_STROKE_BLACK = "replace_stroke:#000"
-REPLACE_FILL_BLACK = "replace_fill:#000"
+REPLACE_STROKE_WHITE = "stroke:#fff"
+REPLACE_FILL_WHITE = "fill:#fff"
+REPLACE_STROKE_BLACK = "stroke:#000"
+REPLACE_FILL_BLACK = "fill:#000"
 
 
 #helper sort xml.sh
@@ -45,7 +53,7 @@ def natural_sort_key(s: str, _nsre=re.compile('([0-9]+)')):
             for text in re.split(_nsre, s.as_posix())]
 
 #xml.sh
-def convert_svg_files(svgdir: str, export: str) -> None:
+def convert_svg_files(iconsdir: str, xmldir: str, valuesdir:str, assetsdir:str,appfilterpath:str) -> None:
     icpack_pre = '\t    <item>'
     icpack_suf = '</item>\n'
     drawable_pre = '\t  <item drawable="'
@@ -57,7 +65,7 @@ def convert_svg_files(svgdir: str, export: str) -> None:
     with open('drawable.xml', 'w',encoding="utf-8") as fp:
         fp.write('<?xml version="1.0" encoding="utf-8"?>\n<resources>\n\t <version>1</version>\n\t  <category title="New" />\n')
 
-    for dir_ in sorted(Path(svgdir).glob('*.svg'), key=natural_sort_key):
+    for dir_ in sorted(Path(iconsdir).glob('*.svg'), key=natural_sort_key):
         file_ = dir_.name
         name = file_[:file_.rindex('.')]
         with open('iconpack.xml', 'a',encoding="utf-8") as fp:
@@ -70,42 +78,40 @@ def convert_svg_files(svgdir: str, export: str) -> None:
     with open('drawable.xml', 'a') as fp:
         fp.write('</resources>\n')
 
-    copy2('iconpack.xml', f"{export}/res/xml/")
-    copy2('iconpack.xml', f"{export}/res/values/")
-    copy2('drawable.xml', f"{export}/res/xml/")
-    copy2('drawable.xml', f"{export}/assets/")
+    copy2('iconpack.xml', xmldir)
+    copy2('iconpack.xml', valuesdir)
+    copy2('drawable.xml', xmldir)
+    copy2('drawable.xml', assetsdir)
+    os.remove('iconpack.xml')
+    os.remove('drawable.xml')
 
-    copy2('appfilter.xml', f"{export}/assets/")
-    copy2('appfilter.xml', f"{export}/res/xml/")
+    copy2(appfilterpath, assetsdir)
+    copy2(appfilterpath, xmldir)
 
-#equivalent white.sh and black.sh
-#rewrite to do both black and white
+#Change Color of SVG based on rules
 def svg_colors(dir:str,stroke:str,fill:str,replace_stroke:str,replace_fill:str)  -> None:
-
-
     for x in glob.glob(f"{dir}/*.svg"):
         with open(x, 'r') as fp:
             content = fp.read()
-
+       
         content = re.sub(stroke, replace_stroke, content, flags=re.IGNORECASE)
         content = re.sub(fill, replace_fill, content, flags=re.IGNORECASE)
-        
+    
         with open(x, 'w') as fp:
             fp.write(content)
 
-#rasterdark     &  #rasterlight.sh
+#Create PNG of the SVG and Copy to destination
 def create_icons(sizes: List[int], dir:str ,export_dir: str, icon_dir: str , mode:str) -> None:
-
     for file_path in glob.glob(f"{dir}/*.svg"):
         file= os.path.basename(file_path)
         name = file[:-4]
-        copy2(file, f'{icon_dir}/{file}')
+        copy2(file_path, f'{icon_dir}/{file}')
         print(f'Working on {file} {mode}')
         for size in sizes:
             subprocess.run(['inkscape', '--export-filename='+f'{name}'+'.png',
-                            f'--export-width={size}', f'--export-height={size}', f'{name}.svg'])
+                            f'--export-width={size}', f'--export-height={size}', file_path])
             if size == 256:
-                copy2(f'{name}.png', f'{export_dir}/drawable-nodpi/')
+                copy2(f'{name}.png', export_dir)
                 Path(f'{name}.png').unlink()
 
 
@@ -113,19 +119,18 @@ def remove_svg(dir:str):
     for file_path in glob.glob(f"{dir}/*.svg"):
         os.remove(file_path)
 
-def create_new_drawables(svgdir: str) -> None:
+def create_new_drawables(svgdir: str,newdrawables:str) -> None:
     drawable_pre = '\t<item drawable="'
     drawable_suf = '" />\n'
-    with open('newdrawables.xml', 'w',encoding="utf-8") as fp:
+    with open(newdrawables, 'w',encoding="utf-8") as fp:
         fp.write('<?xml version="1.0" encoding="utf-8"?>\n<resources>\n\t<version>1</version>\n\t<category title="New" />\n')
         for file_path in glob.glob(f"{svgdir}/*.svg"):
-        #for dir_ in Path(svgdir).glob('**/*.svg'):
             file= os.path.basename(file_path)
             name = file[:-4]
             fp.write(f'{drawable_pre}{name}{drawable_suf}')
         fp.write('</resources>\n')
 
-def merge_new_drawables(pathxml: str, pathnewxml:str):
+def merge_new_drawables(pathxml: str, pathnewxml:str, assetpath:str):
 
     drawables = []
     folder = []
@@ -211,14 +216,14 @@ def merge_new_drawables(pathxml: str, pathnewxml:str):
         # write to new_'filename'.xml in working directory
         outFile = open(pathxml, "w", encoding='utf-8')
         outFile.write(output)
-        copy2(pathxml, ASSETS_PATH)
+        copy2(pathxml, assetpath)
         os.remove(pathnewxml)
 
 #new appfilter sort
-def sortxml(dir:str):
+def sortxml(path:str):
     # Parse the XML file
     parser = etree.XMLParser(remove_blank_text=True)
-    tree = etree.parse(f'{dir}/appfilter.xml', parser)
+    tree = etree.parse(path, parser)
     root = tree.getroot()
     comment_str = None
 
@@ -247,9 +252,38 @@ def sortxml(dir:str):
         root.append(comment)
         root.extend(element[1])
     #Write sorted xml to file
-    tree.write(f'{dir}/appfilter.xml', pretty_print=True)
+    tree.write(path, pretty_print=True)
 
+def find_non_white_svgs(dir: str):
+    non_white_svgs = {}
+    for file_path in glob.glob(f"{dir}/*.svg"):
+        file= os.path.basename(file_path)
+        name = file[:-4]
+        with open(file_path, 'r') as fp:
+            content = fp.read()
+            stroke_colors = re.findall(r'stroke:#.*?(?=;)', content)
+            fill_colors = re.findall(r'fill:#.*?(?=;)', content)
+            for stroke_color in stroke_colors:
+                if stroke_color not in ['stroke:#ffffff', 'stroke:#fff', 'stroke:#FFFFFF']:
+                    if file in non_white_svgs:
+                        non_white_svgs[file] += [stroke_color]
+                    else: non_white_svgs[file] = [stroke_color]
+            for fill_color in fill_colors:
+                if fill_color not in ['fill:#ffffff', 'fill:#fff', 'fill:#FFFFFF']:
+                    if file in non_white_svgs:
+                        non_white_svgs[file] += [fill_color]
+                    else: non_white_svgs[file] = [fill_color]
+    if len(non_white_svgs) > 0:
+        print('______ Found SVG with colors other then white ______\n')
+        for svg in non_white_svgs:
+            print(f'\n{svg}:')
+            for colors in non_white_svgs[svg]:
+                print(f'\t {colors}')
 
+        print("\n____ Please check these first before preceeding ____\n")
+        return True
+
+    return False
 
 
 
@@ -258,17 +292,17 @@ def sortxml(dir:str):
 
 
 def main():
-    create_new_drawables(SVG_DIR)
+    if find_non_white_svgs(SVG_DIR):
+        return
+    create_new_drawables(SVG_DIR,NEWDRAWABLE_PATH)
     svg_colors(SVG_DIR,ORIGINAL_STROKE,ORIGINAL_FILL,REPLACE_STROKE_WHITE,REPLACE_FILL_WHITE)
-    create_icons(SIZES, SVG_DIR ,export_white_dir, icon_dir, 'Dark Mode')
+    create_icons(SIZES, SVG_DIR ,EXPORT_DARK_DIR, WHITE_DIR, 'Dark Mode')
     svg_colors(SVG_DIR,ORIGINAL_STROKE,ORIGINAL_FILL,REPLACE_STROKE_BLACK,REPLACE_FILL_BLACK)
-    create_icons(SIZES, SVG_DIR ,export_black_dir, icon2_dir, 'Light Mode')
+    create_icons(SIZES, SVG_DIR ,EXPORT_LIGHT_DIR, BLACK_DIR, 'Light Mode')
     remove_svg(SVG_DIR)
-    sortxml(SVG_DIR)
-    convert_svg_files(icon_dir, export_xml_dir) 
-    merge_new_drawables(f'{SVG_DIR}/app/res/xml/drawable.xml',f'{SVG_DIR}/newdrawables.xml')
-    
-    #black_svg_colors(SVG_DIR)
+    sortxml(APPFILTER_PATH)
+    convert_svg_files(WHITE_DIR, RES_XML_PATH,VALUE_PATH,ASSETS_PATH,APPFILTER_PATH) 
+    merge_new_drawables(DRAWABLE_PATH,NEWDRAWABLE_PATH,ASSETS_PATH)
 
 
 if __name__ == "__main__":
