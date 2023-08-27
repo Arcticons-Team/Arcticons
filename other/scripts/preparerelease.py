2ab37aa64322facb8b20b5ff0401fa8fcc27d28a
Bastian <8929967+Kaiserdragon2@users.noreply.github.com>
2023-08-27 13:08:47 +0200

    change spaces to taps for xml icons

--- other/scripts/preparerelease.py
+++ other/scripts/preparerelease.py
@@ -106,9 +106,6 @@ def svg_xml_exporter(dir:str,exportpath:str,icon_dir:str,mode:str):
 
     def svg_to_xml(svg_file, xml_file):
         all_style ={}
-        # Load the SVG file
-        #with open(svg_file, 'r') as file:
-        #    svg_content = file.read()
 
         # Extract the path data and style information from the SVG
         paths, attributes = svg2paths(svg_file)
@@ -116,14 +113,14 @@ def svg_xml_exporter(dir:str,exportpath:str,icon_dir:str,mode:str):
         
         # Start building the XML file
         xml = '<adaptive-icon xmlns:android="http://schemas.android.com/apk/res/android">\n'
-        xml += '    <background android:drawable="@color/icon_background_color" />\n'
-        xml += '        <foreground>\n'
-        xml += '            <inset android:inset="25%">\n'
-        xml += '            <vector\n'
-        xml += '                android:width="48dp"\n'
-        xml += '                android:height="48dp"\n'
-        xml += '                android:viewportWidth="48"\n'
-        xml += '                android:viewportHeight="48">\n'
+        xml += '\t<background android:drawable="@color/icon_background_color" />\n'
+        xml += '\t<foreground>\n'
+        xml += '\t\t<inset android:inset="25%">\n'
+        xml += '\t\t\t<vector\n'
+        xml += '\t\t\t\tandroid:width="48dp"\n'
+        xml += '\t\t\t\tandroid:height="48dp"\n'
+        xml += '\t\t\t\tandroid:viewportWidth="48"\n'
+        xml += '\t\t\t\tandroid:viewportHeight="48">\n'
 
         # Add a path for each element in the SVG
         for path, attr in zip(paths, attributes):
@@ -166,45 +163,45 @@ def svg_xml_exporter(dir:str,exportpath:str,icon_dir:str,mode:str):
 
 
             if not rotate == None:
-                xml +='                <group\n'
-                xml += f'                   android:rotation="{rotate}"'
+                xml +='\t\t\t\t<group\n'
+                xml += f'\t\t\t\t\tandroid:rotation="{rotate}"'
                 if not transX == None:
-                    xml += f'\n                   android:translateX="{transX}"'
+                    xml += f'\n\t\t\t\t\tandroid:translateX="{transX}"'
                 if not transY == None:
-                    xml += f'\n                   android:translateY="{transY}">\n'
+                    xml += f'\n\t\t\t\t\tandroid:translateY="{transY}">\n'
                 else:
                     xml +='>\n'
             
-            xml += '                <path\n'
+            xml += '\t\t\t\t<path\n'
             if not (fill == 'none' or fill == None):
-                xml += f'                   android:fillColor="@color/icon_color"\n'
+                xml += f'\t\t\t\t\tandroid:fillColor="@color/icon_color"\n'
                 if fill_opacity == None:
                     fillO_match = re.search(fillO_pattern, str(attr))
                     if fillO_match:
                         fill_opacity = fillO_match.group('FillO')
                 if not (fill_opacity == None):
-                    xml += f'                   android:fillAlpha="{fill_opacity}"\n'
+                    xml += f'\t\t\t\t\tandroid:fillAlpha="{fill_opacity}"\n'
             if not (stroke == 'none' or stroke == None):    
-                xml += f'                   android:strokeColor="@color/icon_color"\n'
+                xml += f'\t\t\t\t\tandroid:strokeColor="@color/icon_color"\n'
                 if stroke_opacity == None:
                     strokeO_match = re.search(strokeO_pattern, str(attr))
                     if strokeO_match:
                         stroke_opacity = strokeO_match.group('StrokeO')
                 if not (stroke_opacity == None):
-                    xml += f'                   android:strokeAlpha="{stroke_opacity}"\n'
-            xml += f'                   android:strokeWidth="1.2"\n'
-            xml +=  '                   android:strokeLineCap="round"\n'
-            xml +=  '                   android:strokeLineJoin="round"\n'
-            xml +=  '                   android:pathData="'
+                    xml += f'\t\t\t\t\tandroid:strokeAlpha="{stroke_opacity}"\n'
+            xml += f'\t\t\t\t\tandroid:strokeWidth="1.2"\n'
+            xml +=  '\t\t\t\t\tandroid:strokeLineCap="round"\n'
+            xml +=  '\t\t\t\t\tandroid:strokeLineJoin="round"\n'
+            xml +=  '\t\t\t\t\tandroid:pathData="'
             xml += path.d()
             xml += '"/>\n'
             if not rotate == None:
-                xml +='                </group>\n'
+                xml +='\t\t\t\t</group>\n'
 
         # Close the XML file
-        xml += '            </vector>\n'
-        xml += '        </inset>\n'
-        xml += '    </foreground>\n'
+        xml += '\t\t\t</vector>\n'
+        xml += '\t\t</inset>\n'
+        xml += '\t</foreground>\n'
         xml += '</adaptive-icon>\n'
 
         # Write the XML to the output file
@@ -616,7 +613,6 @@ def main():
     sortxml(APPFILTER_PATH)
     convert_svg_files(WHITE_DIR, RES_XML_PATH,VALUE_PATH,ASSETS_PATH,APPFILTER_PATH) 
     merge_new_drawables(DRAWABLE_PATH,NEWDRAWABLE_PATH,ASSETS_PATH)
-
 
 if __name__ == "__main__":
 	main()
