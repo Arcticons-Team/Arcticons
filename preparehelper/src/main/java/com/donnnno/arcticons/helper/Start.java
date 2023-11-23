7a43dfa66f3d834e2ecdc8c4d64dc215ec69deb8
Bastian <8929967+Kaiserdragon2@users.noreply.github.com>
2023-11-23 19:02:43 +0100

    check root dir

--- preparehelper/src/main/java/com/donnnno/arcticons/helper/Start.java
+++ preparehelper/src/main/java/com/donnnno/arcticons/helper/Start.java
@@ -1,16 +1,29 @@
 package com.donnnno.arcticons.helper;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Start {
 
     public static void main(String[] args) {
+        //String
+        String rootDir = System.getProperty("user.dir");
+        // Get the path of the root directory
+        Path rootPath = Paths.get(rootDir);
+        // Get the name of the root directory
+        String rootDirName = rootPath.getFileName().toString();
+        if (rootDirName.equals("preparehelper")){
+            rootDir = "..";
+        }
+        String sourceDir = rootDir + "/icons/white";
+        String resDir;
+        String destDir;
+        System.out.println("root Dir: " + rootPath);
+        System.out.println("root Dir Name: " + rootDirName);
         if (args.length > 0) {
             String flavor = args[0];
             // Use the flavor as needed
             System.out.println("Processing with flavor: " + flavor);
-            String rootDir = "..";
-            String sourceDir = rootDir + "/icons/white";
-            String resDir;
-            String destDir;
             //String appFilterFile = rootDir + "/app/assets/appfilter.xml";
             switch (flavor) {
                 case "you":
