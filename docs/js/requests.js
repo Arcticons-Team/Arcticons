cc985e088b45994388489273fdc72dbeb1dc9038
Bastian <8929967+Kaiserdragon2@users.noreply.github.com>
2024-03-15 19:24:32 +0100

    fix link

--- docs/js/requests.js
+++ docs/js/requests.js
@@ -111,7 +111,7 @@ const updatableButton = document.getElementById("updatable-button");
 // Add an event listener to the button
 updatableButton.addEventListener("click", function() {
     // Define the URL to redirect to
-    const updatableURL = `https://${RepoOwner}.github.io/${RepoName}/requests.html`;
+    const updatableURL = `https://${RepoOwner}.github.io/${RepoName}/updatable.html`;
     // Redirect to the specified URL
     window.location.href = updatableURL;
 });
