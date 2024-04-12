628abc26093a10418388d6cfabd53d9d3cf245a9
Bastian <8929967+Kaiserdragon2@users.noreply.github.com>
2024-04-12 17:12:11 +0200

    try having combined appfilter in main

--- docs/js/updatable.js
+++ docs/js/updatable.js
@@ -56,7 +56,7 @@ fetch(`https://raw.githubusercontent.com/${RepoOwner}/${RepoName}/${RepoBranch}/
         updateHeaderText(`${appEntriesData.length} Possible Appfilter Updates`);
 
         // Example usage:
-        fetch(`https://raw.githubusercontent.com/${RepoOwner}/${RepoName}/icon-requests/docs/assets/combined_appfilter.xml`)
+        fetch(`assets/combined_appfilter.xml`)
             .then(response => {
                 if (!response.ok) {
                     // If appfilter.xml cannot be loaded, render appEntriesData as is
