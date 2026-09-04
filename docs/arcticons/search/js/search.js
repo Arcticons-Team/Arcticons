43e9453c2c85c7f9140dd28f215cf441d7134fcb
Bastian <8929967+Kaiserdragon2@users.noreply.github.com>
2026-09-04 15:41:14 +0200

    Don't modify original Array.
    Fix #3569

--- docs/arcticons/search/js/search.js
+++ docs/arcticons/search/js/search.js
@@ -83,7 +83,8 @@ function renderGrid() {
   }
 }
 
-function shuffleArray(arr) {
+function shuffleArray(originalarr) {
+    const arr = [...originalarr];
     for (let i = arr.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [arr[i], arr[j]] = [arr[j], arr[i]]; // Swap elements
